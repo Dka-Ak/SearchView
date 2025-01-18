448e03df321b59c44c3cbc6d943e1a072aa6641f
hitor- 1 <154870205+akishore15@users.noreply.github.com>
2025-01-18 14:25:22 -0800

    Add files via upload

--- search.py
+++ search.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify
 from googlesearch import search
-import os
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)  # Secret key for session management
 
 @app.route('/search', methods=['POST'])
 def perform_search():
@@ -11,12 +9,7 @@ def perform_search():
     query = data.get('query', '')
 
     if query:
-        session['search_query'] = query
         results = search(query, num_results=10)  # Number of results can be adjusted
-
-        # End the session after search
-        session.clear()
-
         return jsonify(results)
     return jsonify([])
 
